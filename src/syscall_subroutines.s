40c9e965789c401d1a60adec515a3c02e0fc1e06
polebytes <polebyte0x7@gmail.com>
2024-04-30 16:53:18 +0530

    `syscall`: add write syscall
    Signed-off-by: polebytes <polebyte0x7@gmail.com>

--- src/syscall_subroutines.s
+++ src/syscall_subroutines.s
@@ -4,13 +4,26 @@
 	ret
 
 /*
-	exit - exit from the program
+	exit -> exit from the program
 	@rdi - status_code of the return value
 */
 .global _sys_exit
 _sys_exit:
 	mov $60, %rax
 	jmp ._syscall
+
+/*
+	write -> outputs something on the screen
+	@rdi: fd;
+	@rdi: void address; address to a null-termination string
+	@rdx: count; amount of bytes to write on the screen
+	@returns: -1 on error, number of bytes written on the screen when success
+*/
+.global _sys_write
+_sys_write:
+	mov $1, %rax
+	jmp ._syscall
+
 
 
 
